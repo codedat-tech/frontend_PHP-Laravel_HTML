04a0afaee3e0726ac3a49136421019669410c0ba
Dhanupa <danupadanupa@gmail.com>
2024-09-04 22:47:37 +0530

    Laravel PHP CRUD Project - Initial Setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('admin-login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login.form');
+Route::post('admin-login', [AdminAuthController::class, 'login'])->name('admin.login');
+
+// Define route for index view
+Route::get('index', function () {
+    return view('index');
+})->name('index');
+
+Route::resource('products', ProductController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
