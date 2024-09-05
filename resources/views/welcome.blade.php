109c8ac29fb41fa19c6b59220b87a0b3321a47a0
Dhanupa <danupadanupa@gmail.com>
2024-09-05 09:15:33 +0530

    Fix CSS structure and properly link stylesheet in HTML

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Yummy Admin Panel</title>
+    <title>Welcome to Admin Panel</title>
     <link rel="stylesheet" href="{{ asset('css/admin-login.css') }}">
 </head>
 <body>
     <div class="container">
-        <h1>Welcome to the Yummy Admin Panel</h1>
+        <h1>Welcome to the Admin Panel</h1>
         <a href="{{ route('index') }}" class="btn">Go to Dashboard</a>
     </div>
 </body>
